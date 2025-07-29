514368a14b6bec8c8757b92e830849fdfd5fc08b
haja <hei.haja.@gmail.com>
2025-07-29 13:32:28 +0300

    add class , constructor and getter

---
+++ src/main/java/weAreBroke/Expense.java
@@ -0,0 +1,27 @@
+package weAreBroke;
+
+import java.time.LocalDate;
+
+public class Expense {
+    private String label;
+    private double amount;
+    private LocalDate date;
+
+    public Expense(String label, double amount, LocalDate date) {
+        this.label = label;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+}
